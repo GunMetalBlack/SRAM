c6814a38c215c61232ce991fd49b6b535dedcf8c
GunMetalBlack <powerhousegames206@gmail.com>
2023-05-30 17:12:29 -0400

    s

--- Assets/Scripts/PlayerNetwork.cs
+++ Assets/Scripts/PlayerNetwork.cs
@@ -2,29 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Unity.Netcode;
+using TMPro;
 public class PlayerNetwork : NetworkBehaviour
 {
-// Creates network variable for the player data with readable everyone and Only server and Owner Can Edit
+    //Some Scetch Vars For UI
+    TextMeshProUGUI attackDefendTextMesh;
+    // Creates network variable for the player data with readable everyone and Only server and Owner Can Edit
     private NetworkVariable<PlayerData> NetworkPlayerData = new NetworkVariable<PlayerData> (
     new PlayerData
     {
-        IsPlayerTurn = true,
         Energy = 0,
         Health = 0,
 
     },NetworkVariableReadPermission.Everyone,NetworkVariableWritePermission.Owner);
 
+    private NetworkVariable<bool> NetworkPlayerTurn = new NetworkVariable<bool>(true,NetworkVariableReadPermission.Everyone,NetworkVariableWritePermission.Owner);
+
     // Player Data Networking Stores All Major Stats 
     public struct PlayerData : INetworkSerializable
     {
-        public bool IsPlayerTurn;
         public int Energy;
 
         public int Health;
 
         public void NetworkSerialize<T>(BufferSerializer<T> serializer) where T : IReaderWriter
         {
-           serializer.SerializeValue(ref IsPlayerTurn);
            serializer.SerializeValue(ref Energy);
            serializer.SerializeValue(ref Health);
         }
@@ -34,14 +36,26 @@ public class PlayerNetwork : NetworkBehaviour
     {
         NetworkPlayerData.OnValueChanged += (PlayerData oldValue, PlayerData newValue) =>
         {
-             Debug.Log(OwnerClientId + ": Old Network, Value: " + oldValue.IsPlayerTurn + "; " + oldValue.Energy + "; " + oldValue.Health);
-            Debug.Log(OwnerClientId + ": Network, Value: " + newValue.IsPlayerTurn + "; " + newValue.Energy + "; " + newValue.Health);
+             Debug.Log(OwnerClientId + ": Old Network, Value: " +  "; " + oldValue.Energy + "; " + oldValue.Health);
+            Debug.Log(OwnerClientId + ": Network, Value: " + "; " + newValue.Energy + "; " + newValue.Health);
         };
+        NetworkPlayerTurn.OnValueChanged += (bool wasTurn, bool IsTurn)=>
+        {
+            if(IsTurn == true)
+            {
+                attackDefendTextMesh.text = "Attack";
+            }
+            else
+            {
+                attackDefendTextMesh.text = "Defend";
+            }
+        };
+        //Cursed On Init Values
         if(IsOwner)
         {
+            //Default Network Values?
             NetworkPlayerData.Value = new PlayerData
             {
-               IsPlayerTurn = false,
                Energy = 20,
                Health = 100,
             };
@@ -53,16 +67,21 @@ public class PlayerNetwork : NetworkBehaviour
     void Update()
     {
         if(!IsOwner) return;
+        //Test Code For Update Network Player Data
         if(Input.GetKeyDown(KeyCode.T))
         {
             NetworkPlayerData.Value = new PlayerData
             {
-               IsPlayerTurn = true,
                Energy = 20,
                Health = 100,
             };
         }
         
+    }
+    [ServerRpc]
+    private void PlayerTurnServerRpc(bool IsTurn)
+    {
+        NetworkPlayerTurn.Value = IsTurn;
     }
 }
 
