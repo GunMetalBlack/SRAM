0cfb390e3b528620f4dacbac172ecdec40beaf45
GunMetalBlack <powerhousegames206@gmail.com>
2023-05-31 22:58:34 -0400

    HUGE HUGE HUGE WE HAVE MULTIPLAYER MAYBE

--- Assets/NetworkManagerUI.cs
+++ Assets/NetworkManagerUI.cs
@@ -5,23 +5,6 @@ using UnityEngine.UI;
 using Unity.Netcode;
 public class NetworkManagerUI : MonoBehaviour
 {
-    [SerializeField] private Button serverBtn;
-    [SerializeField] private Button hostBtn;
-    [SerializeField] private Button clientBtn;
-    
-    private void Awake() {
-       serverBtn.onClick.AddListener(()=>{
-            NetworkManager.Singleton.StartServer();
-            gameObject.SetActive(false);
-       }); 
-         hostBtn.onClick.AddListener(()=>{
-            NetworkManager.Singleton.StartHost();
-            gameObject.SetActive(false);
-       }); 
-         clientBtn.onClick.AddListener(()=>{
-            NetworkManager.Singleton.StartClient();
-            gameObject.SetActive(false);
-       }); 
-    }
+
 
 }
--- Assets/Scripts/Relay.cs
+++ Assets/Scripts/Relay.cs
@@ -4,22 +4,73 @@ using UnityEngine;
 using Unity.Services.Core;
 using Unity.Services.Authentication;
 using Unity.Services.Relay;
-
+using Unity.Services.Relay.Models;
+using Unity.Netcode;
+using Unity.Netcode.Transports.UTP;
+using TMPro;
 public class Relay : MonoBehaviour
 {
+    [SerializeField] GameObject NetUI;
+    [SerializeField] TextMeshProUGUI joinCodeText;
+     [SerializeField] TextMeshProUGUI joinCodeTextInput;
+    public string JoinCodeFromInput;
     // Start is called before the first frame update
     private async void Start()
     {
-       await UnityServices.InitializeAsync();
-        AuthenticationService.Instance.SignedIn += () =>{
+        await UnityServices.InitializeAsync();
+        AuthenticationService.Instance.SignedIn += () =>
+        {
             Debug.Log("Signed In" + AuthenticationService.Instance.PlayerId);
         };
-       await AuthenticationService.Instance.SignInAnonymouslyAsync();
+        await AuthenticationService.Instance.SignInAnonymouslyAsync();
 
     }
 
-    private void CreateRelay(){
-        RelayService.Instance.CreateAllocationAsync();
+    public async void CreateRelay()
+    {
+        try
+        {
+            Allocation allocation = await RelayService.Instance.CreateAllocationAsync(1);
+
+            string joinCode = await RelayService.Instance.GetJoinCodeAsync(allocation.AllocationId);
+            joinCodeText.text = "JoinCode:"+ joinCode;
+            Debug.Log("Join Code: " + joinCode);
+            NetworkManager.Singleton.GetComponent<UnityTransport>().SetHostRelayData(
+               allocation.RelayServer.IpV4,
+               (ushort)allocation.RelayServer.Port,
+               allocation.AllocationIdBytes,
+               allocation.Key,
+               allocation.ConnectionData,
+               true
+            );
+            NetworkManager.Singleton.StartHost();
+            NetUI.SetActive(false);
+        }
+        catch (RelayServiceException e) { Debug.Log(e); }
+       
+       
+    }
+    public void setJoinCode(){
+        JoinCodeFromInput = joinCodeTextInput.text;
+        Debug.Log("Join Code: " + JoinCodeFromInput);
+    }
+    public async void JoinRelay()
+    {
+        try
+        {
+            JoinAllocation joinAllocation = await RelayService.Instance.JoinAllocationAsync(JoinCodeFromInput);
+            NetworkManager.Singleton.GetComponent<UnityTransport>().SetClientRelayData(
+                joinAllocation.RelayServer.IpV4,
+                (ushort)joinAllocation.RelayServer.Port,
+                joinAllocation.AllocationIdBytes,
+                joinAllocation.Key,
+                joinAllocation.ConnectionData,
+                joinAllocation.HostConnectionData
+            );
+            NetworkManager.Singleton.StartClient();
+            NetUI.SetActive(false);
+        }
+        catch (RelayServiceException e) { Debug.Log(e);  }
     }
 
 }
